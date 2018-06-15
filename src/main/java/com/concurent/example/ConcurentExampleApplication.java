9f09a50200f119b99848136ad8db8e42429bda9d
ezskama <mateusz.skalbania@ericsson.com>
2018-06-15 16:43:36 +0200

    Initial

---
+++ src/main/java/com/concurent/example/ConcurentExampleApplication.java
@@ -0,0 +1,12 @@
+package com.concurent.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurentExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurentExampleApplication.class, args);
+	}
+}
---
+++ src/test/java/com/concurent/example/ConcurentExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.concurent.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConcurentExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
